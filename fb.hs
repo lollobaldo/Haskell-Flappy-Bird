9082c367b3d62cf13eda7ee9e79729167fb12df1
Lorenzo <lollobaldo2000@gmail.com>
2019-03-04 18:51:12 +0000

    final 2.0

--- fb.hs
+++ fb.hs
@@ -10,14 +10,15 @@ import Graphics.Gloss.Interface.IO.Game
 
 data FbGame = FbGame{
     birdPos :: Float,
-    birdVel :: Float,
+    birdSpeed :: Float,
     birdStatus :: Int,
     barSlit :: Float,
     barPos :: Float,
+    barSpeed :: Float,
+    bgPos :: Float,
     rds :: [Float],
     score :: Int,
-    dead :: Bool,
-    pause :: Bool,
+--  0:Start, 1:Normal, 2:Pause, 3:Dead, 4:Cheat
     gameMode :: Int} deriving Show
 
 color_bar  = green
@@ -28,7 +29,7 @@ force_do = 3
 
 fps = 60
 
-pos_window = (100,100)
+pos_window = (50,50)
 
 size_x_bar  = 75
 size_x_bart = 95
@@ -41,8 +42,8 @@ size_y_window = 800
 
 speed_bar  = -200
 speed_bird = 300
-speed_delta = -40
-speed_grav = 450
+speed_delta = -15
+speed_grav = 500
 
 title = "Flappy Bird in Haskell!!!"
 
@@ -52,39 +53,30 @@ background = white
 initialState :: [Float] -> FbGame
 initialState fs = FbGame {
     birdPos=0,
-    birdVel=0,
+    birdSpeed=0,
     birdStatus = 0,
     barSlit=0,
     barPos=0,
+    barSpeed=speed_bar,
+    bgPos=0,
     rds=fs,
     score = 0,
-    dead=False,
-    pause=False,
     gameMode=0}
 
-bg0 :: Picture
-bg0 = unsafePerformIO . loadBMP $ "bg0.bmp"
 
-bg1 :: Picture
-bg1 = unsafePerformIO . loadBMP $ "bg1.bmp"
+getSprite :: String -> FilePath
+getSprite name = "sprites/" ++ name ++ ".bmp"
 
-birdAlive :: Picture
-birdAlive = scale 0.5 0.5 . unsafePerformIO . loadBMP $ "bird0.bmp"
-
-birdDead :: Picture
-birdDead = scale 0.5 0.5 . unsafePerformIO . loadBMP $ "bird1.bmp"
-
-botBar :: Picture
-botBar = scale 2 2 . unsafePerformIO . loadBMP $ "bot.bmp"
-
-gameOver :: Picture
-gameOver = scale 3 3 . unsafePerformIO . loadBMP $ "go.bmp"
-
-pausePic :: Picture
-pausePic = scale 5 5 . unsafePerformIO . loadBMP $ "pause.bmp"
-
-topBar :: Picture
-topBar = scale 2 2 . unsafePerformIO . loadBMP $ "top.bmp"
+bg0 = unsafePerformIO . loadBMP . getSprite $ "bg0"
+bg1 = translate 512 0 . unsafePerformIO . loadBMP . getSprite $ "bg1"
+birdAlive = scale 0.5 0.5 . unsafePerformIO . loadBMP . getSprite $ "bird0"
+birdDead = scale 0.5 0.5 . unsafePerformIO . loadBMP . getSprite $ "bird1"
+botBar = scale 2 2 . unsafePerformIO . loadBMP . getSprite $ "bot"
+fbPic = translate 0 200 . scale 3 3 . unsafePerformIO . loadBMP . getSprite $ "fb"
+gameOver = scale 3 3 . unsafePerformIO . loadBMP . getSprite $ "go"
+pausePic = scale 5 5 . unsafePerformIO . loadBMP . getSprite $ "pause"
+tapPic = translate (-130) (-55) . scale 2 2 . unsafePerformIO . loadBMP . getSprite $ "tap"
+topBar = scale 2 2 . unsafePerformIO . loadBMP . getSprite $ "top"
 
 window :: Display
 window = InWindow title (round size_x_window, round size_y_window) pos_window
@@ -102,23 +94,28 @@ main = do
     ranTup = (0, (size_y_window -size_a_slit -300))
     update :: Float -> FbGame -> FbGame 
     update sec game
-        | dead game = game
-        | pause game = game
-        | otherwise = detectCrushBar . detectCrushFloor . moveBar sec . movePlayer sec $ game
+        | gameMode game == 1 || gameMode game == 4 = detectCrushBar . detectCrushFloor . moveBg sec . moveBar sec . movePlayer sec $ game
+        | otherwise = game
 
 handleKeys :: Event -> FbGame -> FbGame
-handleKeys (EventKey (SpecialKey KeySpace) (Down) _ _) game = game {birdVel = speed_bird}
+handleKeys (EventKey (SpecialKey KeySpace) (Down) _ _) game
+    | gameMode game == 0 = game {birdSpeed = speed_bird, gameMode = 1}
+    | gameMode game == 1 = game {birdSpeed = speed_bird, gameMode = 1}
+    | gameMode game == 2 = game {birdSpeed = speed_bird, gameMode = 1}
+    | gameMode game == 3 = (initialState (rds game)) {birdSpeed = speed_bird, gameMode = 1}
+    | gameMode game == 4 = game {birdSpeed = speed_bird, gameMode = 1}
 handleKeys (EventKey (Char 'r') (Down) _ _) game = initialState (rds game)
-handleKeys (EventKey (Char 'l') (Down) _ _) game = game {gameMode = 1}
-handleKeys (EventKey (Char 'p') (Up) _ _) game =
-    if (pause game)
-        then game {pause=False}
-        else game {pause=True}
+handleKeys (EventKey (Char 'l') (Down) _ _) game
+    | gameMode game == 4 = game {gameMode = 1}
+    | otherwise = game {gameMode = 4}
+handleKeys (EventKey (Char 'p') (Down) _ _) game
+    | gameMode game == 2 = game {gameMode = 1}
+    | otherwise = game {gameMode = 2}
 handleKeys _ game = game
 
 detectCrushFloor :: FbGame -> FbGame
 detectCrushFloor game
-    | birdPos game < -250 = game {birdStatus=3, dead=True}
+    | birdPos game < -250 = game {birdStatus=3, gameMode=3}
     | otherwise = game
 
 detectCrushBar :: FbGame -> FbGame
@@ -126,9 +123,7 @@ detectCrushBar game
     | (abs (barsP + (3*size_x_window/4))) < size_a_bird + size_a_bars
    && (birdP + size_a_bird > barsS + size_y_window/2 - 100
     || birdP - size_a_bird < barsS - size_a_slit + size_y_window/2 -100)
-        = game {birdStatus=3, dead=True}
-    -- | birdP == barsP = game {score = scoreNew}
-    -- | abs(birdP-barsP) <3 = game {score = (score game)+1}
+        = game {birdStatus=4, gameMode=3}
     | otherwise = game
   where
     birdP = birdPos game
@@ -142,57 +137,69 @@ debugger'' (a,b,c) = show a ++ " - " ++
 
 render :: FbGame -> Picture
 render game
-    | dead game = pictures [standard, gameOver]
-    | pause game = pictures [standard, pausePic]
+    | gameMode game == 0 = pictures [standard, fbPic, tapPic]
+    | gameMode game == 2 = pictures [standard, pausePic]
+    | gameMode game == 3 = pictures [standard, gameOver]
     | otherwise = pictures [standard]
   where
     standard = pictures [
         bg0,
         bar (barPos game) (barSlit game),
-        bg1,
+        bg (bgPos game),
         bird (birdPos game) (birdStatus game),
+        --debug game,
         scoreGen (score game)]
 
 movePlayer :: Float -> FbGame -> FbGame
 movePlayer sec game
-    | gameMode game == 1 = game{birdPos = size_y_window/2 - 100 - barSlit game - size_a_slit/2}
+    | gameMode game == 4 = game{birdPos = size_y_window/2 - 100 - barSlit game - size_a_slit/2}
     | otherwise = game {
     birdPos = p',
-    birdVel = v',
+    birdSpeed = v',
     birdStatus = s'}
   where
-    (p,v) = (birdPos game, birdVel game)
-    (p', v', s') = (p + v'*sec, v - speed_grav*sec, if v'>0 then 1 else 0)
+    (p,v) = (birdPos game, birdSpeed game)
+    (p', v', s') = (p + v'*sec, v - speed_grav*sec, if v'>0 then 2 else 1)
 
 moveBar :: Float -> FbGame -> FbGame
 moveBar sec game = game {
     barPos=p',
     barSlit=s',
-    rds =r',
+    barSpeed=v',
+    rds=r',
     score=sc'}
   where
-    (p,  s,  r,  sc) = (barPos game, barSlit game, rds game, score game)
-    (p', s', r', sc') --if p > -size_x_window then (p + speed_bar*sec , s, r) else (0,sNew,rNew)
-        | p > -(size_x_window+size_a_bars*3) = (p + v*sec , s, r,sc)
-        | otherwise           = (0,sNew,rNew,sc+1)
-    v = fromIntegral $ speed_bar+speed_delta*sc
+    (p,  s,  v,  r,  sc) = (barPos game, barSlit game, barSpeed game, rds game, score game)
+    (p', s', v', r', sc') --if p > -size_x_window then (p + speed_bar*sec , s, r) else (0,sNew,rNew)
+        | p > -(size_x_window+size_a_bars*3) = (p + v*sec , s, v+speed_delta*sec, r,sc)
+        | otherwise           = (0,sNew,v+speed_delta*sec,rNew,sc+1)
     sNew = head r
     rNew = tail r
+
+moveBg :: Float -> FbGame -> FbGame
+moveBg sec game = game{bgPos=p'}
+  where
+    (p,v) = (bgPos game, barSpeed game)
+    p'  | p > (-1024)  = p + (v-100)*sec
+        | otherwise    = p + (v-100)*sec +1024
 
 bird :: Float -> Int -> Picture
 bird y s = translate (0-size_x_window/4) y (sprite s)
   where
-    sprite 0 = rotate ( 15) birdAlive
-    sprite 1 = rotate (-15) birdAlive
-    sprite 2 = birdDead
-    sprite 3 = rotate 90 birdDead
+    sprite 0 = birdAlive
+    sprite 1 = rotate ( 15) birdAlive
+    sprite 2 = rotate (-15) birdAlive
+    sprite 3 = birdDead
+    sprite 4 = rotate 90 birdDead
 --bird y = color color_bird $ translate (0-size_x_window/4) y $ circleSolid size_a_bird
 
 bar :: Float -> Float -> Picture
 bar x y = translate x (-y) barGen
 
+bg x = translate x 0 bg1
+
 debug :: FbGame -> Picture
-debug game = pictures [text $ show (score game)]
+debug game = pictures [text $ show (barSpeed game)]
 
 -- take Int relative position of the bottom of the slit:
 -- i.e.: 0: slit bottom at top + 100
@@ -215,7 +222,7 @@ numsSprite :: BitmapData
 numsSprite = pic2bmp nums
 
 nums :: Picture
-nums = unsafePerformIO . loadBMP $ "num.bmp"
+nums = unsafePerformIO . loadBMP . getSprite $ "num"
 
 pic2bmp :: Picture -> BitmapData
 pic2bmp (Bitmap bmpData) = bmpData
